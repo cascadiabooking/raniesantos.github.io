1c379b223e52f3db084d8a8e236fa6fc845d5be1
Ranie Santos <ranietots32@gmail.com>
2016-04-06 05:26:45 +0800

    started new post, added highlighters, updated config

--- _source/scripts/bower.js
+++ _source/scripts/bower.js
@@ -7,5 +7,7 @@
 
 //=require ../../bower_components/prism/prism.js
 //=require ../../bower_components/prism/plugins/show-language/prism-show-language.min.js
-//=require ../../bower_components/prism/components/prism-scss.js
-//=require ../../bower_components/prism/components/prism-php.js
+//=require ../../bower_components/prism/components/prism-markdown.min.js
+//=require ../../bower_components/prism/components/prism-php.min.js
+//=require ../../bower_components/prism/components/prism-scss.min.js
+//=require ../../bower_components/prism/components/prism-yaml.min.js
