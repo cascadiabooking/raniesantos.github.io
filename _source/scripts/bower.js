e400a9493e81536c7d22e5b022dbb0230bf58cf3
Ranie Santos <ranietots32@gmail.com>
2016-05-24 09:14:31 +0800

    fixed script compile error

--- _source/scripts/bower.js
+++ _source/scripts/bower.js
@@ -2,7 +2,7 @@
 // Bower Components
 // ==========================================================================
 //=require ../../bower_components/jquery/dist/jquery.min.js
-//=require ../../bower_components/history.js/scripts/bundled/html4+html5/jquery.history.js
+//=require ../../bower_components/history.js/scripts/bundled-uncompressed/html4+html5/jquery.history.js
 //=require ../../bower_components/jquery_appear/jquery.appear.js
 
 //=require ../../bower_components/prism/prism.js
